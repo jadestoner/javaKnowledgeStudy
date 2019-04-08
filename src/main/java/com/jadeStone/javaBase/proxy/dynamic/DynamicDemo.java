0a18b02d449c8aef253ae3d6edeea1e04066b6fa
yxy <18915808090@163.com>
2019-04-08 14:22:31 +0800

    简单实现了一个RPC远程调用的动态代理，只代理接口的用法

--- src/main/java/com/jadeStone/javaBase/proxy/dynamic/DynamicDemo.java
+++ src/main/java/com/jadeStone/javaBase/proxy/dynamic/DynamicDemo.java
@@ -13,9 +13,13 @@ public class DynamicDemo {
 		doo.doo();
 		doo = (DoService)new ProxyFactory(doo).getInstence();
 		doo.doo();
+		
+		// 网上看到一个有意思的用法（RPC代理），只代理接口，这里实现以下
+		// 场景：客户端只有一个DoService接口，调用doo方法时，反射时调用远程方法即可
+		doo = (DoService)new ProxyOnlyInterface(DoService.class).getInstence();
+		doo.doo();
 	}	
 }
-
 
 class ProxyFactory{
 	private Object o;
@@ -42,3 +46,31 @@ class ProxyFactory{
 	}
 }
 
+class ProxyOnlyInterface{
+	// 这是一个接口的class对象，如上文的DoService.class
+	private Class o;
+	public ProxyOnlyInterface(Class o) throws Exception{
+		this.o = o;
+	}
+	
+	public Object getInstence(){
+		return java.lang.reflect.Proxy.newProxyInstance(
+				o.getClassLoader(),
+				new Class[]{o},
+				new InvocationHandler(){
+					@Override
+					public Object invoke(Object proxy, Method method,
+							Object[] args) throws Throwable {
+						System.out.println("pre,代理的接口名称"+o.getName()+",接口方法"+method.getName());
+						// 这里就不能再使用method.invoke方法了，因为这里的o不是实例对象
+						// 使用场景是一个RPC的调用,这里将接口名，方法名，参数，版本，等信息拼装成一个对象，序列化后发送出去即可实现简单的RPC代理调用
+//						Object a = method.invoke(o, args);
+						Object result = "远程调用返回的结果";
+//						result = 远程调用返回的结果
+						System.out.println("post");
+						return result;
+					}
+					
+				});
+	}
+}
